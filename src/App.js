33ddecc78765b7a712a487e095cb3e4cd192841b
krunal rathod <krunalrathod992@gmil.com>
2026-01-02 08:59:23 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+import React from 'react';
+
+import Nevbar from './Component/Nevbar';
+import Comment from './Component/comment';
+// import P from './Component/p';
+// import { Routes, Route } from 'react-router-dom';
+// import Userapi from './Component/Userapi';
+// import Table from './Component/Table';
+// import Timer from './Component/Timer';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+     {/* <P/>
+     <Routes>
+   
+      
+      <Route path='/useapi' element={<Userapi/>}/>
+      <Route path='/Table' element={<Table/>}/>
+      <Route path='/timer' element={<Timer/>}/>
+     
+     </Routes>  */}
+     {/* <Nevbar/> */}
+     {/* <Comment/> */}
+     <Nevbar/>
+     {/* <Comment/> */}
+
+   
     </div>
   );
 }
-
-export default App;
